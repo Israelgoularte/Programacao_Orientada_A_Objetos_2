8ed7dcd63672c18b09bab559b9398f60346c1897
Israel Goularte <105762574+Israelgoularte@users.noreply.github.com>
2022-10-24 19:30:22 -0300

    ex4

---
+++ src/Threed/Sub/Primos.java
@@ -0,0 +1,37 @@
+public class Primos{
+    private int min;
+    private int max;
+
+
+    public Primos(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+    
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return this.max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public boolean ePrimo(int e)
+    {
+        for(int i = mim;i <=max;i++)
+        {
+            if(ePrimo(i))
+                sout
+        }
+    }
+
+}
